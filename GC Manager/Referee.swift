b7824296852e30776dc14c0c2a60b079c4b7854d
Sam Spencer <contact@iraremedia.com>
2015-12-13 18:05:43 -0500

    Added Referee Swift Class
    The new “Referee” class in Swift will become the refactored form of the
    Obj-C GameCenterManager class.

---
+++ GC Manager/Referee.swift
@@ -0,0 +1,15 @@
+//
+//  Referee.swift
+//  GameCenterManager
+//
+//  Created by Sam Spencer on 12/13/15.
+//  Copyright © 2015 NABZ Software. All rights reserved.
+//
+
+
+import Foundation
+import GameKit
+
+class Referee: NSObject {
+
+}
